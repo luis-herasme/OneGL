7370534ab8b46623f61cc2cccad04354870ce3b8
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2025-02-25 00:15:53 -0400

    Refactor WebGL attribute type definitions to use enum for improved type safety and clarity

--- src/attributes.ts
+++ src/attributes.ts
@@ -55,54 +55,56 @@ export function getAttributes<A extends Attributes>({
   };
 }
 
-const FLOAT = 0x1406;
-const FLOAT_VEC2 = 0x8b50;
-const FLOAT_VEC3 = 0x8b51;
-const FLOAT_VEC4 = 0x8b52;
+enum WebGLAttributeType {
+  FLOAT = 0x1406,
+  FLOAT_VEC2 = 0x8b50,
+  FLOAT_VEC3 = 0x8b51,
+  FLOAT_VEC4 = 0x8b52,
 
-const INT = 0x1404;
-const INT_VEC2 = 0x8b53;
-const INT_VEC3 = 0x8b54;
-const INT_VEC4 = 0x8b55;
+  INT = 0x1404,
+  INT_VEC2 = 0x8b53,
+  INT_VEC3 = 0x8b54,
+  INT_VEC4 = 0x8b55,
 
-const UNSIGNED_INT = 0x1405;
-const UNSIGNED_INT_VEC2 = 0x8dc6;
-const UNSIGNED_INT_VEC3 = 0x8dc7;
-const UNSIGNED_INT_VEC4 = 0x8dc8;
+  UNSIGNED_INT = 0x1405,
+  UNSIGNED_INT_VEC2 = 0x8dc6,
+  UNSIGNED_INT_VEC3 = 0x8dc7,
+  UNSIGNED_INT_VEC4 = 0x8dc8,
 
-const BOOL = 0x8b56;
-const BOOL_VEC2 = 0x8b57;
-const BOOL_VEC3 = 0x8b58;
-const BOOL_VEC4 = 0x8b59;
+  BOOL = 0x8b56,
+  BOOL_VEC2 = 0x8b57,
+  BOOL_VEC3 = 0x8b58,
+  BOOL_VEC4 = 0x8b59,
 
-const FLOAT_MAT2 = 0x8b5a;
-const FLOAT_MAT3 = 0x8b5b;
-const FLOAT_MAT4 = 0x8b5c;
+  FLOAT_MAT2 = 0x8b5a,
+  FLOAT_MAT3 = 0x8b5b,
+  FLOAT_MAT4 = 0x8b5c,
+}
 
-const WEBGL_TO_ATTRIBUTE_TYPE = {
-  [FLOAT]: "float",
-  [FLOAT_VEC2]: "vec2",
-  [FLOAT_VEC3]: "vec3",
-  [FLOAT_VEC4]: "vec4",
+const WEBGL_TO_ATTRIBUTE_TYPE: Record<WebGLAttributeType, AttributeTypeLabel> = {
+  [WebGLAttributeType.FLOAT]: "float",
+  [WebGLAttributeType.FLOAT_VEC2]: "vec2",
+  [WebGLAttributeType.FLOAT_VEC3]: "vec3",
+  [WebGLAttributeType.FLOAT_VEC4]: "vec4",
 
-  [INT]: "int",
-  [INT_VEC2]: "ivec2",
-  [INT_VEC3]: "ivec3",
-  [INT_VEC4]: "ivec4",
+  [WebGLAttributeType.INT]: "int",
+  [WebGLAttributeType.INT_VEC2]: "ivec2",
+  [WebGLAttributeType.INT_VEC3]: "ivec3",
+  [WebGLAttributeType.INT_VEC4]: "ivec4",
 
-  [UNSIGNED_INT]: "uint",
-  [UNSIGNED_INT_VEC2]: "uvec2",
-  [UNSIGNED_INT_VEC3]: "uvec3",
-  [UNSIGNED_INT_VEC4]: "uvec4",
+  [WebGLAttributeType.UNSIGNED_INT]: "uint",
+  [WebGLAttributeType.UNSIGNED_INT_VEC2]: "uvec2",
+  [WebGLAttributeType.UNSIGNED_INT_VEC3]: "uvec3",
+  [WebGLAttributeType.UNSIGNED_INT_VEC4]: "uvec4",
 
-  [BOOL]: "bool",
-  [BOOL_VEC2]: "bvec2",
-  [BOOL_VEC3]: "bvec3",
-  [BOOL_VEC4]: "bvec4",
+  [WebGLAttributeType.BOOL]: "bool",
+  [WebGLAttributeType.BOOL_VEC2]: "bvec2",
+  [WebGLAttributeType.BOOL_VEC3]: "bvec3",
+  [WebGLAttributeType.BOOL_VEC4]: "bvec4",
 
-  [FLOAT_MAT2]: "mat2",
-  [FLOAT_MAT3]: "mat3",
-  [FLOAT_MAT4]: "mat4",
+  [WebGLAttributeType.FLOAT_MAT2]: "mat2",
+  [WebGLAttributeType.FLOAT_MAT3]: "mat3",
+  [WebGLAttributeType.FLOAT_MAT4]: "mat4",
 } as const;
 
 function getAttributeTypeLabel(type: number) {
@@ -145,15 +147,15 @@ type AttributeTypeLabel = keyof AttributeTypeMap;
 export type GetAttributeType<T extends AttributeTypeLabel> = AttributeTypeMap[T];
 
 const ATTRIBUTE_SETTERS = {
-  [FLOAT]: floatAttribSetterGenerator(1),
-  [FLOAT_VEC2]: floatAttribSetterGenerator(2),
-  [FLOAT_VEC3]: floatAttribSetterGenerator(3),
-  [FLOAT_VEC4]: floatAttribSetterGenerator(4),
+  [WebGLAttributeType.FLOAT]: floatAttribSetterGenerator(1),
+  [WebGLAttributeType.FLOAT_VEC2]: floatAttribSetterGenerator(2),
+  [WebGLAttributeType.FLOAT_VEC3]: floatAttribSetterGenerator(3),
+  [WebGLAttributeType.FLOAT_VEC4]: floatAttribSetterGenerator(4),
 
-  [INT]: intAttribSetterGenerator(1),
-  [INT_VEC2]: intAttribSetterGenerator(2),
-  [INT_VEC3]: intAttribSetterGenerator(3),
-  [INT_VEC4]: intAttribSetterGenerator(4),
+  [WebGLAttributeType.INT]: intAttribSetterGenerator(1),
+  [WebGLAttributeType.INT_VEC2]: intAttribSetterGenerator(2),
+  [WebGLAttributeType.INT_VEC3]: intAttribSetterGenerator(3),
+  [WebGLAttributeType.INT_VEC4]: intAttribSetterGenerator(4),
 } as const;
 
 function getAttributeSetter(type: number) {
@@ -179,7 +181,7 @@ function floatAttribSetterGenerator(size: number) {
     return function floatAttribSetter(value: BufferOptions) {
       gl.bindBuffer(gl.ARRAY_BUFFER, value.buffer);
       gl.enableVertexAttribArray(index);
-      gl.vertexAttribPointer(index, size, FLOAT, value.normalize || false, value.stride || 0, value.offset || 0);
+      gl.vertexAttribPointer(index, size, gl.FLOAT, value.normalize || false, value.stride || 0, value.offset || 0);
       gl.vertexAttribDivisor(index, value.divisor || 0);
     };
   };
@@ -190,7 +192,7 @@ function intAttribSetterGenerator(size: number) {
     return function intAttribSetter(value: BufferOptions) {
       gl.bindBuffer(gl.ARRAY_BUFFER, value.buffer);
       gl.enableVertexAttribArray(index);
-      gl.vertexAttribIPointer(index, size, INT, value.stride || 0, value.offset || 0);
+      gl.vertexAttribIPointer(index, size, gl.INT, value.stride || 0, value.offset || 0);
       gl.vertexAttribDivisor(index, value.divisor || 0);
     };
   };
